759a414e4f99c5839c54f0abd66bc8c5f33ff109
amartya13 <amartya13.haldar@gmail.com>
2026-01-11 11:37:21 -0500

    Now finding contours

--- ObjectTracking.py
+++ ObjectTracking.py
@@ -12,9 +12,12 @@ while True:
     upper_blue = np.array([130, 255, 255])
     mask = cv.inRange(hsv, lower_blue, upper_blue)
     res = cv.bitwise_and(frame, frame, mask= mask)
+    ret, thresh = cv.threshold(mask, 127, 255, 0)
+    contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    cv.drawContours(frame, contours, -1, (0, 255, 0), 3)
     cv.imshow('frame', frame)
     cv.imshow('mask', mask)
-    cv.imshow('res', res)5
+    cv.imshow('res', res)
     ke = cv.waitKey(5) & 0xFF
     if ke == 27:
         break
