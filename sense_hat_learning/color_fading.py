07c6d2577bcea81d0bbff3ac9a9682673d1d434a
amartya2013 <145404703+amartya2013@users.noreply.github.com>
2026-03-28 05:15:13 -0900

    Add color fading effect to Sense HAT

---
+++ sense_hat_learning/color_fading.py
@@ -0,0 +1,12 @@
+from sense_hat import SenseHat
+import time
+
+sense = SenseHat()
+while True:
+    for i in range(255):
+        sense.clear((255,i,0))
+        time.sleep(0.01)
+
+    for i in range(255):
+        sense.clear((255,255 - i,0))
+        time.sleep(0.01)
