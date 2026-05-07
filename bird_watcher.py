54f78e74681bcebc6e216c404d671105fe50b8d0
amartya13 <amartya13.haldar@gmail.com>
2026-05-07 17:19:17 -0400

    New Revisions

--- bird_watcher.py
+++ bird_watcher.py
@@ -3,10 +3,12 @@ from ultralytics import YOLO
 from datetime import datetime
 import os
 from emailer import send_email
+from ai_response import get_response
+import threading
 
 
 print("Loading YOLO model...")
-model = YOLO("yoloe-11s-seg.pt")
+model = YOLO("yoloe-11s-seg.onnx")
 print("Done Loading Model")
 
 
@@ -18,8 +20,16 @@ for name in names:
         os.mkdir(name.replace(" ", "_"))
 
 
-model.set_classes(names, model.get_text_pe(names))
-capture = cv2.VideoCapture(2)
+# model.set_classes(names, model.get_text_pe(names))
+capture = cv2.VideoCapture(0)
+
+def process_bird(image_path):
+    response = get_response("Tell me about the bird that is in the image?", image_path)
+    send_email(
+        "lopamudra.boston@gmail.com",
+        "Bird Watcher",
+        f"There's a bird in the yard! \n\n {response}",
+    )
 
 while True:
     ret, frame = capture.read()
@@ -31,18 +41,12 @@ while True:
             class_id = int(box.cls[0])  # numeric class (e.g., 0, 1, 2)
             label = result.names[class_id]  # human-readable label (e.g., "person")
 
-            if label == "blue bird":
-                if len(os.listdir("blue_bird")) < 20:
-                    cv2.imwrite(f"blue_bird/bluebird_{datetime.now()}.jpg", frame)
-                    send_email("lopamudra.boston@gmail.com", "Bird Watcher", "There's a bird in the yard! ")
-            if label == "red bird":
-                if len(os.listdir("red_bird")) < 20:
-                    cv2.imwrite(f"red_bird/redbird_{datetime.now()}.jpg", frame)
-                    send_email("lopamudra.boston@gmail.com", "Bird Watcher", "There's a bird in the yard! ")
-            if label == "black bird":
-                if len(os.listdir("black_bird")) < 20:
-                    cv2.imwrite(f"black_bird/blackbird_{datetime.now()}.jpg", frame)
-                    send_email("lopamudra.boston@gmail.com", "Bird Watcher", "There's a bird in the yard! ")
+            if label:
+                if len(os.listdir(f"{label.replace(" ", "_")}")) < 200:
+                    name = f"{label.replace(" ", "_")}/{label.replace(" ", "")}_{datetime.now()}.jpg"
+                    cv2.imwrite(name, frame)
+                    process_bird(name)
+
         annotated_frame = result.plot(boxes=True, masks=False)
         cv2.imshow("Camera", annotated_frame)
         if cv2.waitKey(1) == ord("q"):
--- model_export.py
+++ model_export.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLOE
 
 # Load the PyTorch model
-model = YOLOE("yoloe-11s-seg.pt")
+model = YOLOE("yoloe-11l-seg.pt")
 
 # Define your specific prompts that you want to bake into the model
 names = ["blue bird", "black bird", "red bird"]
@@ -10,4 +10,4 @@ names = ["blue bird", "black bird", "red bird"]
 model.set_classes(names, model.get_text_pe(names))
 
 # Export model as .onnx format with specified resolution (must be a multiple of 32)
-model.export(format="onnx", imgsz=320)
+model.export(format="onnx", imgsz=160)
